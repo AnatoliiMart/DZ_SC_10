e9ce2f629fab51756db4b2089753d42954e520f3
Anatolii <99074480+AnatoliiMart@users.noreply.github.com>
2023-04-10 19:22:24 +0200

    Добавьте файлы проекта.

---
+++ Briefcase.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DZ_SC_10
+{
+    delegate void Act(Briefcase briefcase, Item item);
+    delegate string InfoBiriefcase(Briefcase briefcase);
+    internal class Briefcase 
+    {
+        private string? color;
+        private string? manufacturer;
+        private string? textile;
+        private int     weight;
+        public int      volume   { get; private set; }
+        public List<Item>? items { get; private set; }
+
+
+        public Briefcase(string? color, string? manufactured, string? textile, int weight, int volume, List<Item> items)
+        {
+            
+            this.color        = color;
+            this.manufacturer = manufactured;
+            this.textile      = textile;
+            this.weight       = weight;
+            this.volume       = volume;
+            this.items        = items;
+            this.weight       = weight;
+        }
+
+        public event Act? InCase = (Briefcase briefcase, Item item) =>
+        {
+            Console.WriteLine("Adding new item in briefcase:");
+            item.InputItemData();
+
+            if (briefcase.volume >= item.itemVolume)
+            {
+                briefcase.items?.Add(item);
+                briefcase.volume -= item.itemVolume;
+
+                Console.WriteLine($"To briefcase put item {item.name} with a volume {item.itemVolume}\n");
+                Console.WriteLine($"Remaining volume in the briefcase:\t{briefcase.volume}\n");
+            }
+            else
+                throw new Exception("Not enough volume!!!");
+        };
+
+        public event InfoBiriefcase? InfoBiriefcase = (Briefcase briefcase) =>
+        {
+            return briefcase.ToString();
+        };
+
+        public void PutItemInBriefcase(Item item)
+        {
+              InCase?.Invoke(this, item);
+        }
+
+        public string? PrintInfo()
+        {
+            return InfoBiriefcase?.Invoke(this);
+        }
+
+        public override string ToString()
+        {
+            return "Briefcase color:\t\t" + color + "\nManufacturer:\t\t\t" + manufacturer + "\nTextil:\t\t\t\t" + textile
+                    + "\nWeight:\t\t\t\t" + weight + " kg" + "\nBriefcase volume remaining:\t" +
+                    volume + " l." + "\nAmount of items in briefcase:\t"+ items?.Count;
+        }
+    }
+    class Item 
+    {
+        public string? name       { get; private set; }
+        public int     itemVolume { get; private set; }
+        
+        public Item()
+        {
+            name       = null;
+            itemVolume = 0;
+        }
+        public Item(string name, int itemVolume) 
+        {
+            this.name       = name;
+            this.itemVolume = itemVolume;
+        }
+
+        public void InputItemData()
+        {
+            Console.Write("Enter item name:\t");
+            name = Console.ReadLine();
+
+            Console.Write("Enter item volume:\t");
+            itemVolume = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        public override string ToString() 
+        {
+          return $"Item name:\t{name}\nItem volume:\t{itemVolume} l.";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,170 @@
+﻿using System.Drawing;
+using System;
+
+namespace DZ_SC_10
+{
+    internal class Program
+    {
+        delegate string? RainbowRGB(string? color);
+
+        static RainbowRGB GetRGB                = (string? color) =>
+        {
+            
+            int r;
+            int g;
+            int b;
+            if (color != null)
+                switch (color.ToLower())
+                {
+                    case "red":
+                        r = 255;
+                        g = 0;
+                        b = 0;
+                        return r.ToString() + " , " + g.ToString() + " , " + b.ToString();
+                    case "orange":
+                        r = 255;
+                        g = 127;
+                        b = 0;
+                        return r.ToString() + " , " + g.ToString() + " , " + b.ToString();
+                    case "yellow":
+                        r = 255;
+                        g = 255;
+                        b = 0;
+                        return r.ToString() + " , " + g.ToString() + " , " + b.ToString();
+                    case "green":
+                        r = 0;
+                        g = 255;
+                        b = 0;
+                        return r.ToString() + " , " + g.ToString() + " , " + b.ToString();
+                    case "cyan":
+                        r = 0;
+                        g = 0;
+                        b = 255;
+                        return r.ToString() + " , " + g.ToString() + " , " + b.ToString();
+                    case "indigo":
+                        r = 46;
+                        g = 43;
+                        b = 95;
+                        return r.ToString() + " , " + g.ToString() + " , " + b.ToString();
+                    case "violet":
+                        r = 139;
+                        g = 0;
+                        b = 255;
+                        return r.ToString() + " , " + g.ToString() + " , " + b.ToString();
+                    default:
+                        return null;
+                }
+            return null;
+        };
+
+        static Func<int[], int> MultipleOfSeven = (int[] a)       =>
+        {
+            int count = 0;
+            foreach (int i in a)
+                if (i % 7 == 0)
+                    count++;
+            return count;
+        };
+
+        static Func<int[], int> PositiveCount   = (int[] a)       =>
+        {
+            int count = 0;
+            foreach (int i in a)
+                if (i >= 0)
+                    count++;
+            return count;
+        };
+
+        static Action<int[]>    UniqueNegative  = (int[] a)       =>
+        {
+            Console.Write("All unique negative numbers in array:\t\t");
+            bool found;
+            for (int i = 0; i < a.Length; i++)
+            {
+                found = false;
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (i != j && a[i] == a[j])
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found && a[i] < 0) Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+
+        };
+
+        static Action<string>   GivenWord       = (string word)   =>
+        {
+            word = word.ToLower();
+            string[] words = word.Split(' ');
+            Console.Write("Enter the word to find:\t");
+            string? keyWord = Console.ReadLine();
+            int ? count = 0;
+            foreach (var item in words)
+            {
+                if(item == keyWord)
+                    count++;
+            }
+            Console.WriteLine($"In text is {count} word(s) \"{keyWord}\"");
+        };
+
+        static void Main(string[] args)
+        {
+           // Ex1();
+           // Ex2();
+           Ex3_4_5_6();
+        }
+        static void Ex1()
+        {
+            Console.WriteLine("Enter the color of rainbow:");
+            string? str = Console.ReadLine();
+            Console.WriteLine("RGB code of entered color:\t" + GetRGB(str));
+        }
+
+        static void Ex2()
+        {
+            List<Item> items = new List<Item>();
+
+            Briefcase briefcase = new("Red", "Adidas", "Polyester", 5, 20, items);
+
+            Console.WriteLine(briefcase.ToString());
+
+            try
+            {
+                briefcase.PutItemInBriefcase(new Item());
+                briefcase.PutItemInBriefcase(new Item());
+                briefcase.PutItemInBriefcase(new Item());
+                briefcase.PutItemInBriefcase(new Item());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.ToString());
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine(briefcase.PrintInfo());
+        }
+        static void Ex3_4_5_6()
+        {
+            int[] a = { -1, -2, -3, -3, -4, -5, -6, -7, -7, 8, 9, 10, 10, 11, 12, 13, 13, 14, 15, -15 };
+            Console.WriteLine("Elements of array what is multiple of 7 :\t" + MultipleOfSeven(a));
+
+            Console.WriteLine("Amount of positive elements of array:\t\t" + PositiveCount(a));
+
+            UniqueNegative(a);
+
+            string s = "Hello world hello child hello city city city";
+            GivenWord(s);
+        }
+        
+    }
+}
